971a3bb4d953cb44479da5bc9a00c84070365436
Amael Rosales <amael.rosales@oclock.school>
2024-03-08 15:44:38 +0100

    Ajout d'un bouton rechercher

--- src/components/Home/SearchForm/SearchForm.jsx
+++ src/components/Home/SearchForm/SearchForm.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
+import { Button } from 'semantic-ui-react';
 
 import pokemonsNames from '../../../utils/pokemonsNames';
 import {
@@ -21,22 +22,31 @@ const SearchForm = () => {
       onSubmit={(e) => {
         e.preventDefault();
         dispatch(fetchSearchSinglePokemon(searchInputValue));
+        dispatch(changeSearchValue(''));
       }}
     >
-      <Stack id="input__container" fullWidth>
-        <Autocomplete
-          id="input__search"
-          freeSolo
-          fullWidth
-          options={pokemonsNames}
-          renderInput={(params) => (
-            <TextField {...params} placeholder="Salamèche..." fullWidth />
-          )}
-          value={searchInputValue}
-          onChange={(evt, newValue) => {
-            dispatch(changeSearchValue(newValue));
-          }}
-        />
+      <Stack id="input__container">
+        <div className="container_flex">
+          <div className="input_container">
+            <Autocomplete
+              id="input__search"
+              freeSolo
+              fullWidth
+              options={pokemonsNames}
+              renderInput={(params) => (
+                <TextField {...params} placeholder="Salamèche..." fullWidth />
+              )}
+              value={searchInputValue}
+              onChange={(evt, newValue) => {
+                dispatch(changeSearchValue(newValue));
+              }}
+            />
+          </div>
+
+          <Button type="submit" color="black">
+            Rechercher
+          </Button>
+        </div>
       </Stack>
     </form>
   );
